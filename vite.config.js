b72f3aa6a51ab8c0eb27619f5ed211b0a21c08c6
vishnu <vishnuram.nagarajan@gmail.com>
2026-08-22 08:21:04 +0100

    Web site deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Arc-ai-website/',
   plugins: [react()],
   server: {
     host: true,
